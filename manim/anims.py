a29a7dd05dada926490d047f91eac31e752cdb26
Richard Hladík <rihl@uralyx.cz>
2023-06-05 02:17:13 +0200

    Basic voting preference table

--- manim/anims.py
+++ manim/anims.py
@@ -1,10 +1,132 @@
-#manim -pql --fps 15 -r 290,180 anims.py Polylogo
+# manim -pql --fps 15 -r 290,180 anims.py Polylogo
 from random import randrange
 from re import I
 from unittest import skip
 from manim import config as global_config
 from icecream import ic
+import colorsys
 from utils.util_general import *
+
+
+class Fruit(VMobject):
+    def __init__(self, label, normal, gray: VMobject = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label = label
+        self._normal = normal
+        if gray is None:
+            color = normal.get_color()
+            color.saturation = 0
+            gray = normal.copy().set_color(color)
+        self._gray = gray
+        self.add(self._normal)
+
+    def gray(self):
+        self._gray.move_to(self._normal)
+        self._normal.save_state()
+        return self._normal.animate.become(self._gray)
+        return FadeIn(self._gray)
+
+    def ungray(self):
+        return self._normal.animate.restore()
+        return FadeOut(self._gray)
+
+
+FRUITS = {
+    f.label: f
+    for f in (
+        Fruit("A", Circle(color=GREEN).set_fill(GREEN, 1).scale(0.5)),
+        Fruit("B", Square(color=YELLOW).set_fill(YELLOW, 1).scale(0.5)),
+        Fruit("C", Square(color=RED).set_fill(RED, 1).scale(0.5)),
+    )
+}
+
+
+def get_fruit(label):
+    assert label in list("ABC")
+    return FRUITS[label].copy()
+
+
+class Preference(VMobject):
+    def __init__(self, ordering, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.group = VGroup()
+        for label in ordering:
+            fruit = get_fruit(label)
+            self.group.add(fruit)
+        self.ordering = ordering
+        self.group.arrange(DOWN)
+        self.add(self.group)
+
+    def _ix(self, label_or_index):
+        if type(label_or_index) == int:
+            return label_or_index
+        for i, m in enumerate(self.group):
+            if label_or_index == m.label:
+                return i
+        return None
+
+    def at(self, ix):
+        return self.group[self._ix(ix)]
+
+    def gray(self, ix):
+        return self.at(ix).gray()
+
+    def ungray(self, ix):
+        return self.at(ix).ungray()
+
+    def rearrange(self, ordering):
+        self.ordering = ordering
+        positions = list(reversed(sorted(obj.get_y() for obj in self.group)))
+        return [
+            self.at(label).animate.set_y(positions[i])
+            for i, label in enumerate(ordering)
+        ]
+
+    def push_down(self, label):
+        ordering = list(self.ordering)
+        ordering.remove(label)
+        ordering.append(label)
+        return AnimationGroup(*self.rearrange(ordering))
+
+
+def column_broadcast(fn):
+    def inner(self, *args, indexes=None, **kwargs):
+        anims = []
+        for ix in self._ixs(indexes):
+            anims.append(fn(self.group[ix], *args, **kwargs))
+        return anims
+
+    return inner
+
+
+class VotingTable(VMobject):
+    def __init__(self, preferences, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.group = VGroup()
+        for preference in preferences:
+            self.group.add(Preference(preference))
+        self.group.arrange()
+        self.add(self.group)
+
+    def _ixs(self, ixs):
+        if ixs is None:
+            ixs = range(len(self.group))
+        if type(ixs) == int:
+            ixs = [ixs]
+        return ixs
+
+    @column_broadcast
+    def gray(col, label):
+        return col.gray(label)
+
+    @column_broadcast
+    def ungray(col, label):
+        return col.ungray(label)
+
+    @column_broadcast
+    def push_down(col, label):
+        return col.push_down(label)
+
 
 class Polylogo(Scene):
     def construct(self):
@@ -37,9 +159,22 @@ class Polylogo(Scene):
 
         self.play(*[FadeOut(o) for o in self.mobjects])
         self.wait()
-        
+
 
 class Explore(Scene):
     def construct(self):
-        pass
-
+        table = VotingTable(["ABC", "BCA", "ACB"])
+        self.add(table)
+        self.wait(1)
+        self.play(*table.group[0].rearrange("CAB"))
+        self.wait(1)
+        self.play(*table.push_down("A"))
+        self.wait(1)
+        self.play(*table.gray("C"))
+        self.wait(1)
+        self.play(*table.gray("A"))
+        self.wait(1)
+        self.play(*table.ungray("C"))
+        self.wait(1)
+        self.play(*table.ungray("A"))
+        self.wait(1)
