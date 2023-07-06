f4977d6ebf03fab8587e3bffd279ef7c4932fa65
vaclav rozhon <vaclavrozhon@gmail.com>
2023-07-06 10:42:28 -0400

    a bit smaller background img

--- manim/utils/util.py
+++ manim/utils/util.py
@@ -135,7 +135,7 @@ def intro_images(intro=True):
     )  # TODO pridat polylogo na laptop
 
     background = ImageMobject(
-        f"img/{'background-upscaled' if DRAFT == False else 'background'}.png"
+        f"img/{'background_a_bit_smaller.jpg' if DRAFT == False else 'background.png'}"
     ).scale_to_fit_height(config.frame_height)
 
     whiteboard = (
